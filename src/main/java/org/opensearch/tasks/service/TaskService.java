19a92ffb09aaf8ba74970dfbdc03d9dca31ab394
damian.teplitz <damian.teplitz@monnetpayments.com>
2024-07-26 15:30:49 -0400

    Use repository in service

--- src/main/java/org/opensearch/tasks/service/TaskService.java
+++ src/main/java/org/opensearch/tasks/service/TaskService.java
@@ -7,5 +7,33 @@
  */
 package org.opensearch.tasks.service;
 
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.tasks.model.Task;
+import org.opensearch.tasks.repository.TaskRepository;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public void createTask(Task task) throws IOException {
+        taskRepository.createTask(task);
+    }
+
+    public Optional<Task> getTask(String id) throws IOException {
+        return Optional.ofNullable(taskRepository.getTask(id));
+    }
+
+    public void deleteTask(String id) {
+        taskRepository.deleteTask(id);
+    }
+
+    public SearchResponse searchTasks(String query) {
+        return taskRepository.searchTasks(query);
+    }
 }
